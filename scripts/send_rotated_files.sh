727c0afa50d227853cb480d479381eff9321d973
bql20000 <bql20000@gmail.com>
2021-09-19 22:49:39 +0700

    feat: Add script to send rotated files to remote

---
+++ scripts/send_rotated_files.sh
@@ -0,0 +1,7 @@
+HOT_SKEY=${CNODE_HOME}/priv/pool/${POOL_NAME}/hot.skey
+OP_CERT=${CNODE_HOME}/priv/pool/${POOL_NAME}/op.cert
+POOL_DIR=${CNODE_HOME}/priv/pool/${POOL_NAME}
+
+
+scp ${HOT_SKEY} ${CORE_HOST}:${POOL_DIR}
+scp ${OP_CERT} ${CORE_HOST}:${POOL_DIR}
