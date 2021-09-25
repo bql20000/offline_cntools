229460919dad88a705156e3be952ac7e7d0fc073
bql20000 <bql20000@gmail.com>
2021-09-25 19:51:52 +0700

    feat: Split transfer_file.sh to send/get file

---
+++ scripts/send_tmp_file.sh
@@ -0,0 +1,12 @@
+# Please run this inside container
+# Assumption: Transfer file at /tmp/cnode/cntools between local/remote
+
+FILE_TO_TRANSFER=$1
+
+if [ -z "$FILE_TO_TRANSFER" ]
+then
+  echo "error: FILE_TO_TRANSFER to declared"
+  exit 0
+fi
+
+scp ${TMP_DIR}/${FILE_TO_TRANSFER} ${CORE_HOST}:${TMP_DIR}/
