db36cc75dc5dda22f571dc9197a0270b18e94166
bql20000 <bql20000@gmail.com>
2021-12-11 22:25:59 +0700

    feat: Create folder for outdated scripts

---
+++ outdated_scripts/get_backup_from_gcp.sh
@@ -0,0 +1,16 @@
+# outdated since we're not using GCP anymore
+
+# Please run this at root dir (not inside scripts/)
+# Please run this at host (not inside container)
+
+BACKUP_BUCKET=
+
+if [ -z "$BACKUP_BUCKET" ]
+then
+  echo "error: BACKUP_BUCKET not declared"
+  exit 0
+fi
+
+mkdir -p backups
+  
+gsutil rsync -r gs://${BACKUP_BUCKET} backups
